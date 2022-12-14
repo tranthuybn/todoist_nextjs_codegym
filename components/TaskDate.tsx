6088b94f2f9e373289bc40ee3020ad6fe691cbdd
tranthuy <thuydxd01@gmail.com>
2022-12-14 17:38:21 +0700

    update typescript

--- components/TaskDate.tsx
+++ components/TaskDate.tsx
@@ -10,7 +10,9 @@ import { DayPicker } from 'react-day-picker';
 import { useEffect, useState } from 'react';
 import 'react-day-picker/dist/style.css';
 
-function TaskDate({ setTaskDate, setShowTaskDate, showTaskDate }: any) {
+import { TaskDateProps } from '~/interface';
+
+function TaskDate({ setTaskDate, setShowTaskDate, showTaskDate = false }: TaskDateProps) {
   const [selected, setSelected] = useState<Date>();
   useEffect(() => {
     setTaskDate(moment(selected).format('DD/MM/YYYY'));
@@ -18,94 +20,99 @@ function TaskDate({ setTaskDate, setShowTaskDate, showTaskDate }: any) {
   }, [selected]);
 
   return (
-    showTaskDate && (
-      <>
-        <span className="task-date__overlay" onClick={() => setShowTaskDate(!showTaskDate)}></span>
-        <div className="task-date">
-          <ul className="task-date__list">
-            <li>
-              <div
-                onClick={() => {
-                  setShowTaskDate(false);
-                  setTaskDate(moment().format('DD/MM/YYYY'));
-                }}
-                role="button"
-              >
-                <span>
-                  <BsCalendarEvent />
-                </span>
-                <span>Today</span>
-              </div>
-            </li>
-            <li>
-              <div
-                onClick={() => {
-                  setShowTaskDate(false);
-                  setTaskDate(moment().add(1, 'day').format('DD/MM/YYYY'));
-                }}
-                role="button"
-              >
-                <span>
-                  <BsSun />
-                </span>
-                <span>Tomorrow</span>
-              </div>
-            </li>
-            <li>
-              <div
-                role="button"
-                onClick={() => {
-                  setShowTaskDate(false);
-                  setTaskDate(moment().add(7, 'day').format('DD/MM/YYYY'));
-                }}
-              >
-                <span>
-                  <BsBoxArrowRight />
-                </span>
-                <span>Next week</span>
-              </div>
-            </li>
-            <li>
-              <div
-                role="button"
-                onClick={() => {
-                  setShowTaskDate(false);
-                  setTaskDate(moment().day(6).format('DD/MM/YYYY'));
-                }}
-              >
-                <span>
-                  <BsEmojiSmile />
-                </span>
-                <span>This weekend</span>
-              </div>
-            </li>
-            <li>
-              <div
-                role="button"
-                onClick={() => {
-                  setShowTaskDate(false);
-                  setTaskDate('');
-                }}
-              >
-                <span>
-                  <BsDashCircle />
-                </span>
-                <span>No date</span>
-              </div>
-            </li>
-          </ul>
-          <div className="task-date__pick-day">
-            <DayPicker
-              mode="single"
-              disabled={{ before: new Date() }}
-              selected={selected}
-              onSelect={setSelected}
-              onDayClick={() => setShowTaskDate(false)}
-            />
+    <>
+      {showTaskDate && (
+        <>
+          <span
+            className="task-date__overlay"
+            onClick={() => setShowTaskDate(!showTaskDate)}
+          ></span>
+          <div className="task-date">
+            <ul className="task-date__list">
+              <li>
+                <div
+                  onClick={() => {
+                    setShowTaskDate(false);
+                    setTaskDate(moment().format('DD/MM/YYYY'));
+                  }}
+                  role="button"
+                >
+                  <span>
+                    <BsCalendarEvent />
+                  </span>
+                  <span>Today</span>
+                </div>
+              </li>
+              <li>
+                <div
+                  onClick={() => {
+                    setShowTaskDate(false);
+                    setTaskDate(moment().add(1, 'day').format('DD/MM/YYYY'));
+                  }}
+                  role="button"
+                >
+                  <span>
+                    <BsSun />
+                  </span>
+                  <span>Tomorrow</span>
+                </div>
+              </li>
+              <li>
+                <div
+                  role="button"
+                  onClick={() => {
+                    setShowTaskDate(false);
+                    setTaskDate(moment().add(7, 'day').format('DD/MM/YYYY'));
+                  }}
+                >
+                  <span>
+                    <BsBoxArrowRight />
+                  </span>
+                  <span>Next week</span>
+                </div>
+              </li>
+              <li>
+                <div
+                  role="button"
+                  onClick={() => {
+                    setShowTaskDate(false);
+                    setTaskDate(moment().day(6).format('DD/MM/YYYY'));
+                  }}
+                >
+                  <span>
+                    <BsEmojiSmile />
+                  </span>
+                  <span>This weekend</span>
+                </div>
+              </li>
+              <li>
+                <div
+                  role="button"
+                  onClick={() => {
+                    setShowTaskDate(false);
+                    setTaskDate('');
+                  }}
+                >
+                  <span>
+                    <BsDashCircle />
+                  </span>
+                  <span>No date</span>
+                </div>
+              </li>
+            </ul>
+            <div className="task-date__pick-day">
+              <DayPicker
+                mode="single"
+                disabled={{ before: new Date() }}
+                selected={selected}
+                onSelect={setSelected}
+                onDayClick={() => setShowTaskDate(false)}
+              />
+            </div>
           </div>
-        </div>
-      </>
-    )
+        </>
+      )}
+    </>
   );
 }
 export default TaskDate;
